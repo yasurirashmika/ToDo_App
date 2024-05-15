e7da009d4885a9edef25a68d0e47a50112b5c7f2
yasurirashmika <iyasuri.rash@gmail.com>
2024-05-15 23:54:58 +0530

    first commit

---
+++ resources/views/libraries/styles.blade.php
@@ -0,0 +1,8 @@
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+    integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+    
+@stack('css')
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Home
+Route::get('/', [HomeController::class, "index"])->name('home');
+
+//Todo
+Route::prefix('/todo')->group(function(){
+    Route::get('/',[TodoController::class,"index"])->name('todo');
+    Route::post('/store',[TodoController::class,"store"])->name('todo.store');
+    Route::get('/{task_id}/delete',[TodoController::class,"delete"])->name('todo.delete');
+    Route::get('/{task_id}/done',[TodoController::class,"done"])->name('todo.done');
+});
+
